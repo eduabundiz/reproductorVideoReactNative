63dd1e1783c27eaf5a9ca9dc493935c20668d69c
Abundiz <3duabundiz@gmail.com>
2020-04-14 16:51:50 -0500

    Componentes categorías y estilos

--- App.js
+++ App.js
@@ -13,7 +13,22 @@ import {
 import Home from './src/screens/containers/home';
 import Header from './src/sections/components/header';
 import SuggestionList from './src/videos/containers/suggestion-list';
+import API from './utils/api';
+import CategoryList from './src/videos/containers/category-list.js';
 class App extends Component{
+  state ={
+    suggestionList:[],
+    categoryList:[]
+  }
+  async componentDidMount(){
+    const movies = await API.getSuggestion(10);
+    const categories = await API.getMovies();
+    console.log(categories)
+    this.setState({
+      suggestionList :movies,
+      categoryList : categories,
+    })
+  }
   render(){
   return (
     <Home>
@@ -22,7 +37,12 @@ class App extends Component{
       </Header> 
       <Text>Buscador</Text>
       <Text>Categorias</Text>
-      <SuggestionList />
+      <CategoryList 
+      list={this.state.categoryList}
+      />
+      <SuggestionList 
+      list={this.state.suggestionList}
+      />
     </Home>
    );
   }
--- src/videos/containers/suggestion-list.js
+++ src/videos/containers/suggestion-list.js
@@ -9,6 +9,7 @@ import Separator from '../components/verticalSeparator';
 import Suggestion from '../components/suggestion';
 
 class SuggestionList extends Component{
+    keyExtractor = item =>item.id.toString()
     renderEmpty = () => <Empty text="No hay sugerencias :(  "/>
     itemSeparator = () => <Separator color='red' />
     renderItem = ({item}) =>{
@@ -17,23 +18,15 @@ class SuggestionList extends Component{
         )
     }
     render(){
-        const list = [
-            {
-                title:'Avengers',
-                key:'1',
-            },
-            {
-                title: 'Pokemon',
-                key:'2',
-            }
-        ]
+
         return(       
             <Layout
                 title ="Recomendado para ti"
                 
             >
             <FlatList 
-                data ={list}
+                keyExtractor={this.keyExtractor}
+                data ={this.props.list}
                 ListEmptyComponent = {this.renderEmpty}
                 ItemSeparatorComponent={this.itemSeparator}
                 renderItem={this.renderItem}
